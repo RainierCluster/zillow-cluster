e0a540a7c8c9a2c389bc07769663aaa11ccfd14f
Misty <ms.misty.garcia@gmail.com>
2019-11-06 11:48:50 -0600

    add prep with missing values function

---
+++ prep.py
@@ -0,0 +1,9 @@
+import pandas as import pd 
+import numpy as numpy
+
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
