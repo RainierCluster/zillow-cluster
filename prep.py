e6c44ab14f31e4561c32e787c687e0362714061c
Misty <ms.misty.garcia@gmail.com>
2019-11-06 16:19:51 -0600

    built cleaning functions

--- prep.py
+++ prep.py
@@ -7,3 +7,34 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
     threshold = int(round(prop_required_row*len(df.columns),0))
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
+
+def clean_columns(df):
+    """
+    Removing columns that were determined not to provide value
+    """
+    df.drop(columns=["calculatedbathnbr", "fullbathcnt", "finishedsquarefeet12", "regionidcounty", "propertycountylandusecode", "rawcensustractandblock", "censustractandblock", "regionidzip", "regionidcity", "assessmentyear", "propertylandusedesc", "parcelid","roomcnt", "taxvaluedollarcnt"], inplace=True)
+    return df
+
+def drop_minimal_nulls(df):
+    """
+    Drop rows that have that minimal nulls  
+    """
+    df.dropna(axis=0,subset=["calculatedfinishedsquarefeet","structuretaxvaluedollarcnt", "taxvaluedollarcnt","landtaxvaluedollarcnt","taxamount", "yearbuilt"], inplace=True)
+    return df
+
+def impute_lotsize_nulls(df):
+    """
+    Calculate a proportion from lot size and tax value. Disregard outliers, and calculate a mean proportion. Mulitple that mean proportion with the tax value to impute lot sizes for null values. 
+    """
+    # create subset dataframe with lot size and tax value
+    df_subset = df [["lotsizesquarefeet", "taxvaluedollarcnt"]]
+    # create a new column that takes the proportion of lot size to tax value
+    df_subset["proportionlotandvalue"] = df_subset.lotsizesquarefeet.dropna() / df_subset.taxvaluedollarcnt
+    # get the average mean of proportions that are less than 1 and add it to a column
+    mean_proportion = df_subset [df_subset.proportionlotandvalue < 1].proportionlotandvalue.mean()
+    df_subset["mean"] = mean_proportion
+    # fill all the nulls in lot saize with the calculated mean
+    df_subset.lotsizesquarefeet.fillna(df_subset.taxvaluedollarcnt * df_subset["mean"], inplace=True)
+    # replace old lot size with null-free lot size to the original dataframe 
+    df["lotsizesquarefeet"] = df_subset.lotsizesquarefeet.round(0)
+    return df
