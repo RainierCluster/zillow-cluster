cbb2430c01c86ba5478cfc2ac77b8e1d9c451502
Misty <ms.misty.garcia@gmail.com>
2019-11-06 17:07:34 -0600

    final prep functions maybe

--- prep.py
+++ prep.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import numpy as numpy
 
+import warnings
+warnings.filterwarnings("ignore")
+
 def handle_missing_values(df, prop_required_column, prop_required_row):
     threshold = int(round(prop_required_column*len(df.index),0))
     df.dropna(axis=1, thresh=threshold, inplace=True)
@@ -12,7 +15,7 @@ def clean_columns(df):
     """
     Removing columns that were determined not to provide value
     """
-    df.drop(columns=["calculatedbathnbr", "fullbathcnt", "finishedsquarefeet12", "regionidcounty", "propertycountylandusecode", "rawcensustractandblock", "censustractandblock", "regionidzip", "regionidcity", "assessmentyear", "propertylandusedesc", "parcelid","roomcnt", "taxvaluedollarcnt"], inplace=True)
+    df.drop(columns=["calculatedbathnbr", "fullbathcnt", "finishedsquarefeet12", "regionidcounty", "propertycountylandusecode", "rawcensustractandblock", "censustractandblock", "regionidzip", "regionidcity", "assessmentyear", "propertylandusedesc", "parcelid","roomcnt"], inplace=True)
     return df
 
 def drop_minimal_nulls(df):
@@ -37,4 +40,11 @@ def impute_lotsize_nulls(df):
     df_subset.lotsizesquarefeet.fillna(df_subset.taxvaluedollarcnt * df_subset["mean"], inplace=True)
     # replace old lot size with null-free lot size to the original dataframe 
     df["lotsizesquarefeet"] = df_subset.lotsizesquarefeet.round(0)
+    return df
+
+def pretty_cols(df):
+    # better understood column names
+    df = df.rename(columns={"calculatedfinishedsquarefeet":"house_area", "fips":"countyid", "structuretaxvaluedollarcnt":"house_value", "landtaxvaluedollarcnt":"land_value", "taxvaluedollarcnt":"whole_value", "lotsizesquarefeet":"whole_area"})
+    # reorder columns based on relevancy to others
+    df = df[["countyid","latitude","longitude","yearbuilt","bathroomcnt","bedroomcnt","house_area","house_value","whole_area","whole_value","land_value","taxamount","logerror","transactiondate"]]
     return df
